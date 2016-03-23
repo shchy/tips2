a678790b6d7ac85ae79b4adf04f527182a9fe7fa
shchy <shch.pc@gmail.om>
2016-03-23 15:34:40 +0900

    viewTemplate by currentdirectory

--- src/TipsWeb/Startup.cs
+++ src/TipsWeb/Startup.cs
@@ -7,11 +7,22 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Data.Entity;
 using tips2.Models;
+using Nancy;
+using Microsoft.Extensions.PlatformAbstractions;
+using StructureMap;
+using Microsoft.AspNet.Hosting;
 
 namespace tips2
 {
     public class Startup
     {
+        private IContainer Container { get; set; }
+
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            Container = new Container(config => { config.For<IApplicationEnvironment>().Use(appEnv); });
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddEntityFramework()
@@ -21,10 +32,40 @@ namespace tips2
 
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
-            app.UseOwin(x => x.UseNancy());
+            app.UseOwin(x => x.UseNancy(config => config.Bootstrapper = new Bootstrapper(Container)));
         }
 
         // Entry point for the application.
         public static void Main(string[] args) => Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
+    }
+
+
+
+    public class RootPathProvider : IRootPathProvider
+    {
+        public RootPathProvider(IApplicationEnvironment environment)
+        {
+             Environment = environment;
+        }
+
+       private IApplicationEnvironment Environment { get; }
+
+       public string GetRootPath()
+       {
+            return Environment.ApplicationBasePath;
+       }
+    }
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+
+        private readonly IContainer container;
+
+        public Bootstrapper(IContainer container)
+        {
+             this.container = container;
+        }
+
+        protected override IRootPathProvider RootPathProvider => container.GetInstance<RootPathProvider>();
     }
 }
