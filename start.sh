e3330815ef3e909b8d4e78d6c91f6787745f3eaa
shchy <shch.pc@gmail.om>
2016-03-24 15:43:44 +0900

    dbmigtest

--- start.sh
+++ start.sh
@@ -2,12 +2,19 @@
 MIGRATION_TAG=$1
 WEBAPP_PATH="src/TipsWeb"
 MIGRATIONS_PATH="${WEBAPP_PATH}/Migrations"
+FINDED=0
 
+for FILE in `find ${MIGRATIONS_PATH}/*${MIGRATION_TAG}.cs -type f`
+do
+    #Some function on the file
+    FINDED=1
+done
 
-if [ -f "${MIGRATIONS_PATH}/${MIGRATION_TAG}.cs" ]; then
-  echo "Already Exists Migrations"
+if [ $FINDED = 1 ]; then
+    echo "Already Exists Migrations"
 else
     dnx -p ${WEBAPP_PATH}/project.json ef migrations add ${MIGRATION_TAG}
     dnx -p ${WEBAPP_PATH}/project.json ef database update
 fi
+
 dnx -p ${WEBAPP_PATH}/project.json web
